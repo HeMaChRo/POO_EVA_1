523c1bbe0d1a875943222d89f3d855b1d1155082
Hemachro <124392305+HeMaChRo@users.noreply.github.com>
2023-03-05 22:04:49 -0600

    Add files via upload

---
+++ EVA1_14_ALCANCE_3/src/eva1_14_alcance_3/EVA1_14_ALCANCE_3.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_14_alcance_3;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_14_ALCANCE_3 {
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println(x);
+        for (int i = 0; i < 10; i++) {
+            //int x = 100; ya esta declarada en el main
+            System.out.println(i);
+            System.out.println(x);
+            if(i == 5){
+                System.out.println(x);
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);            
+        }
+    }
+    public static void Algo(int x){
+        
+    }
+    
+    public static void Algo2(int x){
+        
+    }
+    
+}
+
