19953c3c4e2840d1c77e03730052b819e7796e75
Hemachro <124392305+HeMaChRo@users.noreply.github.com>
2023-03-05 22:02:26 -0600

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_12_alcance;
+
+public class EVA1_12_ALCANCE {
+    
+    public static void main(String[] args) {
+        System.out.println(x);// no existe la x porque
+                              // esta declarada desupues de la instruccion
+        System.out.println(args); //esta declarada antes de la instruccion
+        int x = 100;
+        System.out.println(x);
+    }
+    
+}
